a65fb5309fcaf3e4d73d9711a4816a5ef302596b
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-10 03:44:19 +0900

    JAVA:toString()

---
+++ src/lang/object/tostring/Dog.java
@@ -0,0 +1,22 @@
+package lang.object.tostring;
+
+public class Dog {
+
+    private String dogName;
+    private int age;
+
+    public Dog(String dogName, int age) {
+        this.dogName = dogName;
+        this.age = age;
+    }
+
+    //toString 오버라이딩
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "dogName='" + dogName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
