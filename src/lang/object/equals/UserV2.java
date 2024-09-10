bde06863f6c1b53cc2a990a571acdcecd0e3f433
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-10 17:58:54 +0900

    JAVA:toString(), equals()

---
+++ src/lang/object/equals/UserV2.java
@@ -0,0 +1,28 @@
+package lang.object.equals;
+
+import java.util.Objects;
+
+public class UserV2 {
+
+    private String id;
+
+    public UserV2(String id) {
+        this.id = id;
+    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        //다운캐스팅 - 객체의 특정 값을 사용하기 위함
+//        UserV2 user = (UserV2) obj;
+//        return id.equals(user.id);
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserV2 userV2 = (UserV2) o;
+        return Objects.equals(id, userV2.id);
+    }
+}
