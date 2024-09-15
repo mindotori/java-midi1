7b12e6e0c9dada7152aaaa3d20aef79a51d77fdf
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-15 22:02:18 +0900

    JAVA:String클래스

---
+++ src/lang/string/StringBasicMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class StringBasicMain {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello"); //객체 생성
+
+        System.out.println("str1 = " +str1);
+        System.out.println("str2 = " +str2);
+    }
+}
