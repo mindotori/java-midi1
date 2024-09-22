82848edbe94e6c62ccf54f539048c38cac25c3f4
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-22 14:20:23 +0900

    JAVA: enum 도입 전

---
+++ src/enumeration/ex1/StringGrade.java
@@ -0,0 +1,8 @@
+package enumeration.ex1;
+
+public class StringGrade {
+    // 상수로 만들기
+    public static final String BASIC = "BASIC";
+    public static final String GOLD = "GOLD";
+    public static final String DIAMOND = "DIAMOND";
+}
