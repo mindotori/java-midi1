62fe46cb76adc740e9d8736165338d5bc62a8662
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-08 04:14:35 +0900

    JAVA: 내부 클래스 활용 예제

--- src/nested/inner/ex1/Engine.java
+++ src/nested/inner/ex1/Engine.java
@@ -9,6 +9,6 @@ public class Engine {
     }
     public void start() {
         System.out.println("충전 레벌 확인: " + car.getChargeLevel());
-        System.out.println(car.getModel() + "의 엔진을 구동합니다.");
+        System.out.println(car.getModel() + "의 엔진을 구동합니다..");
     }
 }
