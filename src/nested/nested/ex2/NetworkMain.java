e6c02f550617aafca6f1655bd0e8f0e1a9894df3
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-08 03:06:51 +0900

    JAVA: 정적 중첩 클래스 리팩토링 후

---
+++ src/nested/nested/ex2/NetworkMain.java
@@ -0,0 +1,12 @@
+package nested.nested.ex2;
+
+
+public class NetworkMain {
+
+    public static void main(String[] args) {
+        //NewtworkMain은 오직 Network 클래스만 사용한다. NetworkMessage 클래스는 전혀 사용하지 않는다.
+        //NetworkMessage는 오직 Network 내부에서만 사용된다.
+        Network network = new Network();
+        network.sendMessage("hello java");
+    }
+}
